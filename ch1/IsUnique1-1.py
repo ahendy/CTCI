b85b47c68024ae00c66c79c0c4bf3f9af6c0d5d5
Austin Hendy <ahendy@hotmail.ca>
2016-09-22 11:21:14 -0700

    Chapter 1 git init

---
+++ ch1/IsUnique1-1.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+def is_unique1(word):
+	"""O(n) space, O(n) runtime """
+	visited = set()
+	for ch in word:
+		if ch in visited:
+			return False
+		visited.add(ch)
+
+	return True
+
+def is_unique2(word):
+	"""Constant space, O(nlogn) runtime"""
+	word = sorted(word)
+	for i in xrange(1, len(word)-1):
+		if word[i-1] == word[i] or word[i+1] == word[i]:
+			return False
+
+	return True
+
+def is_unique3(word):
+	"""Constant Space, O(n^2) runtime"""
+	for ch in word:
+		for ch2 in word:
+			if ch == ch2:
+				return False
+	return True
+
+
+if __name__ == '__main__':
+	
+	assert is_unique1("friend") == True
+	assert is_unique2("friend") == True
+	assert is_unique3("friend") == True
+
+	assert is_unique1("aaa") == False
+	assert is_unique2("aaa") == False
+	assert is_unique3("aaa") == False
